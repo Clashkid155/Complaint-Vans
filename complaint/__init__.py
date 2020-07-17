a4b3201c39183ee10bdde5f3a8380e3e3e67e299
Clashkid155 <frankdiss9@gmail.com>
2020-07-17 15:43:11 +0100

    Use MongoDB

--- complaint/__init__.py
+++ complaint/__init__.py
@@ -1,23 +1,30 @@
 import os
 from flask import Flask
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
 #from .models import User
-
+from flask_mongoengine import MongoEngine
 # init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
+
+
+db = MongoEngine()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
     BASEDIR = os.path.abspath(os.path.dirname(__name__))
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb://Steam:complaints@users-shard-00-00.qnlzb.mongodb.net:27017,users-shard-00-01.qnlzb.mongodb.net:27017,users-shard-00-02.qnlzb.mongodb.net:27017/users?ssl=true&replicaSet=atlas-yzeq0y-shard-0&authSource=admin&retryWrites=true&w=majority",
+        "connect": False
+    }
     app.config['SECRET_KEY'] = 'yu7a8 aiwiiais aus_sisiwi'
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(
-        os.path.join(BASEDIR, "users.db"))
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(
+     #   os.path.join(BASEDIR, "users.db"))
+    #app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    #if os.get
     # init Plugins
     db.init_app(app)
     login_manager.init_app(app)
@@ -31,8 +38,8 @@ def create_app():
         app.register_blueprint(auth.auth)
 
         # Initialize Global db
-        db.create_all()
-        migrate = Migrate(app, db)
+        #db.create_all()
+        #migrate = Migrate(app, db)
 
 #    # blueprint for auth routes in our app
 #    from .auth import auth as auth_blueprint
--- complaint/models.py
+++ complaint/models.py
@@ -1,19 +1,26 @@
 from . import db
+import datetime
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Document):
     """Model for user accounts."""
-    __tablename__ = 'complaint-users'
+    name = db.StringField(required=True)
+    email = db.StringField(required=True, unique=True)
+    password =  db.StringField(required=True)
+    created_on = db.DateTimeField(default=datetime.datetime.utcnow)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False, unique=False)
-    email = db.Column(db.String(40), unique=True, nullable=False)
 
-    password = db.Column(db.String(200), primary_key=False, unique=False, nullable=False)
+    # __tablename__ = 'complaint-users'
+
+    #id = db.Column(db.Integer, primary_key=True)
+    #name = db.Column(db.String, nullable=False, unique=False)
+    #email = db.Column(db.String(40), unique=True, nullable=False)
+
+    #password = db.Column(db.String(200), primary_key=False, unique=False, nullable=False)
     #website = db.Column(db.String(60), index=False, unique=False, nullable=True)
-    created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True)
-    last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
+    #created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True)
+    #last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
 
     def set_password(self, password):
         """Create hashed password."""
@@ -25,3 +32,19 @@ class User(UserMixin, db.Model):
 
     def __repr__ (self):
         return '<User {}>'.format(self.username)
+
+#def init_db():
+#if __name__ == '__main__':
+ #   db.create_all()
+
+    ## Create a test user
+    #new_user = User('a@a.com', 'aaaaaaaa')
+    #new_user.display_name = 'Nathan'
+    #db.session.add(new_user)
+    #db.session.commit()
+
+    # new_user.datetime_subscription_valid_until = datetime.datetime(2019, 1, 1)
+    #db.session.commit()
+
+
+    #init_db()
